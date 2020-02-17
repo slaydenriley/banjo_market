22f6b39dbd5b09cc57c8a88e80fb7b3ca1594205
Riley Slayden <slaydenriley@gmail.com>
2020-02-16 23:11:26 -0600

    Routing for user_controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,38 @@
 class UserController < Sinatra::Base
-    post '/login' do
-        @user = User.find_by(:email => params[:email])
-        erb :"/users/show"
+    get '/login' do
+        if !logged_in?
+          erb :'users/login'
+        else
+          redirect to '/banjos'
+        end
     end
+    
+    post '/login' do
+        user = Users.find_by(:email => params[:email])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect to "/banjos"
+        else
+          redirect to '/signup'
+        end
+    end
+
+    get '/signup' do
+        if !logged_in?
+          erb :'users/new', locals: {message: "Please sign up before you sign in"}
+        else
+          redirect to '/banjos'
+        end
+      end
+    
+      post '/signup' do
+        if params[:name] == "" || params[:email] == "" || params[:password] == ""
+          redirect to '/signup'
+        else
+          @user = Users.new(:name => params[:name], :email => params[:email], :password => params[:password])
+          @user.save
+          session[:user_id] = @user.id
+          redirect to '/banjos'
+        end
+      end
 end
