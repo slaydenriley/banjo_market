7f043a75d8ec2d0db2cc806efd0b32c0ab35a593
Riley Slayden <slaydenriley@gmail.com>
2020-02-17 21:52:42 -0600

    banjo edit routing

--- app/controllers/banjo_controller.rb
+++ app/controllers/banjo_controller.rb
@@ -31,7 +31,7 @@ class BanjoController < ApplicationController
         end
     end
 
-    get '/banjos/:id/' do
+    get '/banjos/:id' do
         @banjo = Banjos.find_by_id(params[:id])
         erb :'banjos/show'
     end
@@ -62,38 +62,22 @@ class BanjoController < ApplicationController
 
     end
 
-    post '/banjos/:id/edit' do
+
+    patch '/banjos/:id' do
         if logged_in?
-            @banjo = Banjos.find_by_id(params[:id])
-            if @banjo.users_id == current_user.id
-                erb :'banjos/edit'
+            if params[:make] == "" || params[:model] = "" || params[:year_made] == "" || params[:price] == ""
+                redirect to 'banjos/:id/edit'
             else
-                redirect to '/banjos'
+                @banjo = Banjos.find_by_id(params[:id])
+                if @banjo.update(make: params[:make], model: params[:model], year_made: params[:year_made], price: params[:price])
+                    redirect to '/banjos/:id'
+                else
+                    redirect to '/banjos/:id/edit'
+                end
             end
         else
             redirect to '/login'
         end
     end
-
-    patch '/banjos/:id/edit' do
-        if logged_in?
-            if params[:make] == "" || params[:model] == "" || params[:year_made] == "" || params[:price] == ""
-                redirect to "/banjos/#{params[:id]}/edit"
-          else
-            @banjo = Banjos.find_by_id(params[:id])
-            if @banjo.id == current_user.id
-              if @banjo.update(:make => params[:make], :model => params[:model], :year_made => params[:year_made], :price => params[:price])
-                redirect to "/banjos/#{@banjo.id}"
-              else
-                redirect to "/banjos/#{@banjo.id}/edit"
-              end
-            else
-              redirect to '/banjos'
-            end
-          end
-        else
-          redirect to '/login'
-        end
-      end
 end
 
